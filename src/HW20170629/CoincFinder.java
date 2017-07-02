1c6f20978eceb103e6f715336797d593c874cc9d
Yuriy Novikov <yunov1@gmail.com>
2017-07-02 18:46:05 +0300

    Homework 170629

---
+++ src/HW20170629/CoincFinder.java
@@ -0,0 +1,34 @@
+package HW20170629;
+
+/**
+ * Created by Yurbly on 30.06.2017.
+ */
+public class CoincFinder {
+    //TODO
+
+    public static int Find(byte[] seq, int w) {
+
+        if(seq.length <= w){
+            return 0;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < seq.length - w; i++) {
+            that:
+            for (int j = i + 1; j <= seq.length - w; j++) {
+                for (int k = 0; k < w; k++) {
+                    if(seq[i + k] != seq[j + k]){
+                        continue that;
+                    }
+
+                }
+                System.out.println("i = " + i + " j = " + j);
+                count++;
+            }
+        }
+
+
+        return count;
+    }
+}
---
+++ src/HW20170629/DNAGenerator.java
@@ -0,0 +1,35 @@
+package HW20170629;
+
+import java.util.Random;
+
+/**
+ * Created by Yurbly on 30.06.2017.
+ */
+public class DNAGenerator {
+
+    static final byte[] alphabet = {'A', 'C', 'G', 'T'};
+
+
+
+    public static byte[] Generate(int i) {
+        Random random = new Random();
+
+        byte[] seq = new byte[i];
+        for (int j = 0; j < i; j++) {
+            seq[j] = alphabet[random.nextInt(4)];
+
+        }
+
+        return seq;
+
+    }
+
+//    static String Generate(int length){
+//
+//        Random random = new Random();
+//
+//        return "";
+//    }
+
+
+}
